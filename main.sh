c2ca90a1b9f7dacff398229fd11b88c1c9a8a9d8
tduck973564 <tduck973564@gmail.com>
2021-08-28 23:10:29 +1000

    added script

---
+++ main.sh
@@ -0,0 +1,7 @@
+while [ : ] 
+do
+	sleep 1500 && zenity --warning --text="Break time"; sleep 300 && zenity --warning --text="Break finished"
+	sleep 1500 && zenity --warning --text="Break time"; sleep 300 && zenity --warning --text="Break finished"
+	sleep 1500 && zenity --warning --text="Break time"; sleep 300 && zenity --warning --text="Break finished"
+	sleep 1500 && zenity --warning --text="Break time"; sleep 900 && zenity --warning --text="Break finished"
+done
